54fe39606fdffd08e361e0bb183386066480f734
Nour Hagag <nourhgag159@gmail.com>
2025-09-09 20:35:17 +0300

    finish app bar

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,20 @@ class Toku extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(body: const Center(child: Text('Tokuuuu app!!'))),
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color(0xff46322B),
+          title: Text(
+            'Toku',
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 25,
+            ),
+          ),
+        ),
+        body: const Center(child: Text('Tokuuuu app!!')),
+      ),
     );
   }
 }
