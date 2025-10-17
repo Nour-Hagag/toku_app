fc0728fea2030b5c783f90b5e2ae879e8418f98e
Nour Hagag <nourhgag159@gmail.com>
2025-10-18 02:26:36 +0300

    add numbers images to pubspec and finish view

--- lib/views/numbers_view.dart
+++ lib/views/numbers_view.dart
@@ -14,7 +14,41 @@ class NumbersView extends StatelessWidget {
         ),
         backgroundColor: const Color(0xff46322B),
       ),
-      body: Text('new view'),
+      body: Container(
+        height: 100,
+        color: Color(0xffF2902F),
+        child: Row(
+          children: [
+            Container(
+              height: 100,
+              width: 100,
+              color: Color(0xffF9EED6),
+              child: Image.asset('assets/images/numbers/number_one.png'),
+            ),
+            SizedBox(width: 5),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Ichi',
+                  style: TextStyle(color: Colors.white, fontSize: 18),
+                ),
+                Text(
+                  'One',
+                  style: TextStyle(color: Colors.white, fontSize: 18),
+                ),
+              ],
+            ),
+            Spacer(flex: 1),
+            IconButton(
+              color: Colors.white,
+              iconSize: 32,
+              onPressed: () {},
+              icon: Icon(Icons.play_arrow_rounded),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
